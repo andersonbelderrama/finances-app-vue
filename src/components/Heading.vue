1ddd2ef474d1bea58ca2a698eefe21c7d9018d24
Anderson Belderrama <andersonbelderrama@gmail.com>
2023-07-22 18:59:10 -0300

    button back page ins heading component

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -20,7 +20,7 @@ export default {
             <h1 class="text-lg uppercase tracking-widest font-semibold">{{ title }}</h1>
         </div>
         <div class="flex space-x-1 items-center">
-            <button class="flex space-x-1 items-center p-2">
+            <button  @click="$router.back()" class="flex space-x-1 items-center p-2">
                 <span class="material-symbols-outlined">arrow_back</span><span>Voltar</span>
             </button>
         </div>
